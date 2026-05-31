6bcfe2e6bf76c5a97780401d139a9d40e9ae5905
Smithnour <96708309+Smithnour@users.noreply.github.com>
2026-05-31 04:25:38 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
+async function generate() {
+    const idea = document.getElementById("idea").value;
 
+    const res = await fetch("/generate", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ idea })
+    });
+
+    const data = await res.json();
+    document.getElementById("output").innerText = data.script;
+}
